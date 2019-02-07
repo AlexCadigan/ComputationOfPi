3607d3c0b61cc56727d8f25cb5fcd60d3807f0ab
Alex Cadigan <cadiganalexander@gmail.com>
2019-02-07 16:55:36 -0500

    Parallelized Parallel function

--- ComputePi.cpp
+++ ComputePi.cpp
@@ -6,13 +6,20 @@ COMP-481 Final Project
 
 #include <cmath>
 #include <iostream>
+#include <omp.h>
 #include "ComputePi.h"
 
 int main() {
-	double N = 10;
+	double N = 1000;
 
+	double start = omp_get_wtime();
 	sequential(N);
+	double timeS = omp_get_wtime() - start;
+	start = omp_get_wtime();
 	parallel(N);
+	double timeP = omp_get_wtime() - start;
+	printf("Sequential Time:\t%f\n", timeS);
+	printf("Parallel Time:\t%f\n", timeP);
 }
 
 void sequential(double N) {
@@ -27,12 +34,17 @@ void sequential(double N) {
 }
 
 void parallel(double N) {
-	double x, y, Pi = 0;
-	for (double i = 1; i <= N; i ++) {
-		x = (1 / N) * (i - 0.5);
-		y = sqrt(1 - pow(x, 2));
-		Pi += 4 * (y / N);
+	double Pi = 0;
+	#pragma omp parallel 
+	{
+		double x, y;
+		#pragma omp for reduction(+:Pi)
+		for (int i = 1; i <= (int) N; i ++) {
+			x = (1 / N) * (i - 0.5);
+			y = sqrt(1 - pow(x, 2));
+			Pi += 4 * (y / N);
+		}
 	}
 
-	printf("SequentialBroken:\t%f\n", Pi);
+	printf("Parallel:\t%f\n", Pi);
 }

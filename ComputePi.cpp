c9c2f039497f68e30eb4f10181778598df093530
Alex Cadigan <cadiganalexander@gmail.com>
2019-02-06 22:31:46 -0500

    Fixed weird error

--- ComputePi.cpp
+++ ComputePi.cpp
@@ -1,12 +1,17 @@
+/*
+Jennifer Cho + Alex Cadigan
+2/6/2019
+COMP-481 Final Project
+*/
+
 #include <cmath>
 #include <iostream>
 #include "ComputePi.h"
 
 int main() {
-	double N = 100000000;
+	double N = 10;
 
 	sequential(N);
-	// sequentialBroken(N);
 	parallel(N);
 }
 
@@ -21,24 +26,13 @@ void sequential(double N) {
 	printf("Sequential:\t%f\n", Pi);
 }
 
-void sequentialBroken(double N) {
-	double x, y, Pi;
+void parallel(double N) {
+	double x, y, Pi = 0;
 	for (double i = 1; i <= N; i ++) {
 		x = (1 / N) * (i - 0.5);
 		y = sqrt(1 - pow(x, 2));
 		Pi += 4 * (y / N);
 	}
 
-	printf("Sequential:\t%f\n", Pi);
-}
-
-void parallel(double N) {
-	double x2, y2, Pi2;
-	for (double i = 1; i <= N; i ++) {
-		x2 = (1 / N) * (i - 0.5);
-		y2 = sqrt(1 - pow(x2, 2));
-		Pi2 += 4 * (y2 / N);
-	}
-
-	printf("Parallel:\t%f\n", Pi2);
+	printf("SequentialBroken:\t%f\n", Pi);
 }
--- ComputePi.h
+++ ComputePi.h
@@ -1,3 +1,8 @@
+/*
+Jennifer Cho + Alex Cadigan
+2/6/2019
+COMP-481 Final Project
+*/
+
 void sequential(double N);
-void sequentialBroken(double N);
 void parallel(double N);

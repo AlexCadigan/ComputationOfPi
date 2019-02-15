56b996ef9810e7c51170c2952136d6db52bf04c3
Alex Cadigan <cadiganalexander@gmail.com>
2019-02-14 21:08:45 -0500

    Committing very broken code

--- ComputePi.cpp
+++ ComputePi.cpp
@@ -10,12 +10,10 @@ COMP-481 Final Project
 
 using namespace std;
 
-void seq(double N);
-void paraInterval(double N, int numThreads);
-void paraIntegral(int intervals, int numThreads);
-void paraMonteCarlo(int num_shots, int numThreads);
-void paraMonteCarloFixed(int num_shots, int numThreads);
-double rnd(unsigned int * seed);
+double seq(double N);
+double paraInterval(int N, int numThreads);
+double paraIntegral(int intervals, int numThreads);
+double paraMonteCarlo(int num_shots, int numThreads);
 
 int main() {
 	// Gets user input:
@@ -26,59 +24,53 @@ int main() {
 	cin >> numCalc;
 
 	// Stores the average times
-	double start, seqAve = 0, intervalAve, integralAve, monteCarloAve;
+	double start, seqAve = 0, intervalAve, integralAve, monteCarloAve, PiSeq, PiInterval, PiIntegral, PiMonte;
 
 	// Runs sequential simulations
 	for (int trialNum = 0; trialNum < numTrials; trialNum ++) {
 		start = omp_get_wtime();
-		seq(numCalc);
+		// PiSeq = seq(numCalc);
 		seqAve += omp_get_wtime() - start;
 	}
-	printf("Sequential Time:\t%f\n", seqAve / numTrials);
-
-
-/*
-Increase the thread amounts for running in Jigwe - find upper limit
-*/
-
-
-
-
+	printf("T_S:\t%f\tE:\t%f%%\n", seqAve / numTrials, (abs(M_PI - PiSeq) / M_PI) * 100);
 	// Runs through the different thread amounts
 	for (int numThreads = 2; numThreads <= 1024; numThreads *= 2) {
 		intervalAve = 0, integralAve = 0, monteCarloAve = 0;
 		// Runs parallel simulations 
 		for (int trialNum = 0; trialNum < numTrials; trialNum ++) {
 			start = omp_get_wtime();
-			paraInterval(numCalc, numThreads);
+			PiInterval = paraInterval(numCalc, numThreads);
 			intervalAve += omp_get_wtime() - start;
 			start = omp_get_wtime();
-			paraIntegral(numCalc, numThreads);
+			PiIntegral = paraIntegral(numCalc, numThreads);
+			printf("Pi Integral:\t%f\n", PiIntegral);
 			integralAve += omp_get_wtime() - start;
 			start = omp_get_wtime();
-			paraMonteCarloFixed(numCalc, numThreads);
+			PiMonte = paraMonteCarlo(numCalc, numThreads);
 			monteCarloAve += omp_get_wtime() - start;
 		}
-		printf("Number of threads:\t%d\tInterval Time:\t%f\tIntegral Time:\t%f\tMonte Carlo Time:\t%f\n", numThreads, intervalAve / numTrials, integralAve / numTrials, monteCarloAve / numTrials);
+		printf("Th:\t%d\tT_Intv:\t%f\tE:\t%f%%\tT_Intg:\t%f\tE:\t%f%%\tT_M:\t%f\tE:\t%f%%\n", numThreads, intervalAve / numTrials, (labs(M_PI - PiInterval) / M_PI) * 100, integralAve / numTrials, (labs(M_PI - PiIntegral) / M_PI) * 100, monteCarloAve / numTrials, (labs(M_PI - PiMonte) / M_PI) * 100);
 	}
 }
 
 /*
 Runs sequential algorithm
 */
-void seq(double N) {
+double seq(double N) {
 	double x, y, Pi = 0;
 	for (double i = 1; i <= N; i ++) {
 		x = (1 / N) * (i - 0.5);
 		y = sqrt(1 - pow(x, 2));
 		Pi += 4 * (y / N);
 	}
+	printf("Pi Seq:\t%f\n", Pi);
+	return Pi;
 }
 
 /*
 Runs parallel interval algorithm
 */
-void paraInterval(double N, int numThreads) {
+double paraInterval(int N, int numThreads) {
 	double Pi = 0;
 	#pragma omp parallel num_threads(numThreads)
 	{
@@ -90,69 +82,43 @@ void paraInterval(double N, int numThreads) {
 			Pi += 4 * (y / N);
 		}
 	}
+	printf("Pi Interval:\t%f\n", Pi);
+	return Pi;
 }
 
 /*
 Runs parallel integral algorithm
 */
-void paraIntegral(int intervals, int numThreads) {
+double paraIntegral(int intervals, int numThreads) {
 	double integral = 0;
-	double dx = 1 / intervals;
+	double dx = 1 / (double) intervals;
 	#pragma omp parallel for num_threads(numThreads) reduction(+:integral) 
 	for (int i = 0; i < intervals; i ++) {
 		double x = i * dx;
 		double fx = sqrt(1 - x * x);
 		integral = integral + fx * dx;
 	}
-	double Pi = 4 * integral;
+	printf("Pi Integral:\t%f\n", 4 * integral);
+	return 4 * integral;
 }
 
 /*
 Runs parallel Monte Carlo algorithm
 */
-
-void paraMonteCarloFixed(int num_shots, int numThreads) {
+double paraMonteCarlo(int num_shots, int numThreads) {
 	int num_hits = 0;
-	unsigned int seed = 42;
-	#pragma omp parallel for num_threads(numThreads) reduction(+:num_hits)
-	for (int shot = 0; shot < num_shots; shot ++) {
-		// unsigned int seed = 42 + omp_get_thread_num();
-		double x = (double) rand_r(& seed) / (double) RAND_MAX;
-		// printf("\nX:\t%f\n", x);
-		double y = (double) rand_r(& seed) / (double) RAND_MAX;
-		// printf("Y:\t%f\n", y);
-		// printf("X * X + Y * Y:\t%f\n", x * x + y * y);
-		if (x * x + y * y <= 1) {
-			num_hits = num_hits + 1;
+	#pragma omp parallel num_threads(numThreads)
+	{	
+		unsigned int seed = omp_get_thread_num();
+		#pragma omp for reduction(+:num_hits)
+		for (int shot = 0; shot < num_shots; shot ++) {
+			double x = (double) rand_r(& seed) / (double) RAND_MAX;
+			double y = (double) rand_r(& seed) / (double) RAND_MAX;
+			if (x * x + y * y <= 1) {
+				num_hits = num_hits + 1;
+			}
 		}
 	}
-	double Pi = 4 * (double) num_hits / (double) num_shots;
-	printf("Pi:\t%f\n", Pi);
-}
-
-void paraMonteCarlo(int num_shots, int numThreads) {
-	unsigned int seeds[numThreads];
-	for (int thread = 0; thread < numThreads; thread ++) {
-		seeds[thread] = thread;
-	}
-	int num_hits = 0;
-	#pragma omp parallel for num_threads(numThreads) reduction(+:num_hits)
-	for (int shot = 0; shot < num_shots; shot ++) {
-		int thread = omp_get_thread_num();
-		double x = rnd(& seeds[thread]);
-		double y = rnd(& seeds[thread]);
-		if (x * x + y * y <= 1) {
-			num_hits = num_hits + 1;
-		}
-	}
-	double Pi = 4 * (double) num_hits / (double) num_shots;
-}
-
-
-/*
-Use rand_r for seed generation
-*/
-double rnd(unsigned int * seed) {
-	* seed = (1140671485 * (* seed) + 12820163) % (1 << 24);
-	return ((double) ( * seed)) / (1 << 24);
+	printf("Pi Monte:\t%f\n", 4 * (double) num_hits / (double) num_shots);
+	return 4 * (double) num_hits / (double) num_shots;
 }
